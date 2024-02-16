ed264bb2f2982abedbeb5c8d146e337ff5b4a703
Özgür Bayram <ozgurbayram@protonmail.com>
2024-02-16 16:01:53 +0300

    Fix error handler

--- src/core/middlewares/error-handler.middleware.ts
+++ src/core/middlewares/error-handler.middleware.ts
@@ -1,19 +1,17 @@
-import {
-  Middleware,
-  ExpressErrorMiddlewareInterface,
-} from "routing-controllers";
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
+import { ExpressErrorMiddlewareInterface, Middleware } from "routing-controllers";
 
 @Middleware({ type: "after" })
 export class ErrorHandler implements ExpressErrorMiddlewareInterface {
-  error(error: any, req: Request, res: Response, next: NextFunction): void {
-    const status = error.httpCode || 500;
+  error(error: Record<string, any>, req: Request, res: Response, next: NextFunction): void {
+    const status = error?.httpCode || 500;
     const message = error.message || "Internal Server Error";
+    const errors = error.errors || null;
 
     res.status(status).json({
       status,
       message,
-      ...error,
+      errors,
     });
   }
 }
